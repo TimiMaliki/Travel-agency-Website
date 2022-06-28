c819b4d85b054901f69c31f2b285e9f2e53d88bb
user.name <malikitimi@gmail.com>
2022-06-28 23:14:11 +0100

    node

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      colors:{
+        'main':'#66DAFF',
+        'second':'#069BCA',
+        'text':'#20284f',
+        'accent':'#Def3ff',
+        'white':'#F9f9f9',
+        'black':'#1c1c1c',
+        'light':'#c0c0c0',
+        'medium':'#787878',
+      }
+    },
+  },
+  plugins: [],
+}
