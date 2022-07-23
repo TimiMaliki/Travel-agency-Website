f6ff259f068acfca73b3c50a8f17dce8be709eb2
user.name <malikitimi@gmail.com>
2022-07-23 13:53:07 +0100

    extra component

--- src/pages/extra/index.js
+++ src/pages/extra/index.js
@@ -1,7 +1,23 @@
-const videoFile = document.getElementId('video-file'),
-videoButton = document.getElementId('video-button'),
-videoIcon = document.getElementId('video-icon'),
+const videoFile = document.getElementById('video-file')
+videoButton = document.getElementById('video-button')
+videoIcon = document.getElementById('video-icon')
 
 function playPause(){
-    if()
+    if(videoFile.paused){
+        //play video
+        videoFile.play()
+
+        // we change the icon
+        videoIcon.classList.add('ri-pause-circle-fill')
+        videoIcon.classList.remove('ri-play-circle-fill')
+    } else{
+        // pause video
+        videoFile.pause()
+
+        //we change the icon
+        videoIcon.classList.remove('ri-pause-circle-fill')
+        videoIcon.classList.add('ri-play-circle-fill')
+    }
 }
+
+videoButton.addEventListener('click', playPause)
