64fb8d0af0cc2f9b7f5bd2080e01991263c139f2
user.name <malikitimi@gmail.com>
2022-07-23 11:15:04 +0100

    extra component

---
+++ src/pages/extra/index.js
@@ -0,0 +1,7 @@
+const videoFile = document.getElementId('video-file'),
+videoButton = document.getElementId('video-button'),
+videoIcon = document.getElementId('video-icon'),
+
+function playPause(){
+    if()
+}
