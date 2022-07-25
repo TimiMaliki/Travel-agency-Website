4b3cd0a408bf5970612d7a25861594bf54097910
user.name <malikitimi@gmail.com>
2022-07-25 19:17:30 +0100

    extra component

--- src/index.js
+++ src/index.js
@@ -6,3 +6,28 @@ btn.addEventListener('click', () =>{
     nav.classList.toggle('flex')
     nav.classList.toggle('hidden')
 });
+
+// video
+const videoFile = document.getElementById('video-file')
+videoButton = document.getElementById('video-button')
+videoIcon = document.getElementById('video-icon')
+
+function playPause(){
+    if(videoFile.paused){
+        //play video
+        videoFile.play()
+
+        // we change the icon
+        videoIcon.classList.add('ri-pause-circle-fill')
+        videoIcon.classList.remove('ri-play-circle-fill')
+    } else{
+        // pause video
+        videoFile.pause()
+
+        //we change the icon
+        videoIcon.classList.remove('ri-pause-circle-fill')
+        videoIcon.classList.add('ri-play-circle-fill')
+    }
+}
+
+videoButton.addEventListener('click', playPause)
